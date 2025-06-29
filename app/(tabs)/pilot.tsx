800303d3fb0b2ae9d5d2b5e64c1fb19df2f1f787
WilhelmSelestam <selestamw@gmail.com>
2025-06-29 17:03:34 +0200

    a

--- app/(tabs)/pilot.tsx
+++ app/(tabs)/pilot.tsx
@@ -1,13 +1,17 @@
 import { View, Text, Button } from "react-native";
 import { useRouter, Link } from "expo-router";
+import { useState } from "react";
 
 export default function Pilot() {
   const router = useRouter();
 
+  const [targetHeading, setTargetHeading] = useState(0);
+
   return (
     <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-accent">Welcome to Pilot</Text>
-      {/* <Link href="../map">Go to map</Link> */}
+
+      <Button title="+"></Button>
     </View>
   );
 }
